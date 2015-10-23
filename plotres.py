70aad9f0f002d7f66c6ab7e5bf1d3b5e66e4347f
Clément Robert <clement.robert@protonmail.com>
2015-10-24 01:13:19 +0200

    corrected mass conversion in data_parser.py

--- plotres.py
+++ plotres.py
@@ -6,14 +6,13 @@ dataf="results.dat"
 tab=np.loadtxt(dataf)
 t,e,l=tab[:,0],tab[:,1],tab[:,2]
 
-e=e/e[0]
-l=l/l[0]
+#e=e/e[0]
+#l=l/l[0]
 
 fig,ax1=pl.subplots()
 ax2=ax1.twinx()
 ax1.plot(t,e,c='b',label=r'$E_{tot}$')
 ax2.plot(t,l,c='r',label=r'$L_{tot}$')
-
 
 ax1.set_xlim(t[0],t[-1])
 ax1.set_xlabel(r'$t$',size=20)
