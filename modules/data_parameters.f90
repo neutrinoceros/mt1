7b7641bb259273c45193ff2d9a29b8d54843afb2
Clement Robert <crobert-imcce@cosmos.imcce.fr>
2015-10-25 14:45:31 +0100

    computed results for 100yr integration

--- modules/data_parameters.f90
+++ modules/data_parameters.f90
@@ -7,6 +7,6 @@ real(8),parameter :: GCST = 1.50528915669e-17 !gravitational constant, here in S
 !computanional parameters
 real(8),parameter :: STEP = 1e-1    ! should be tested with 1/20 * (shorter period (Moon, or alternatively Mercury))
                                    ! for the moon it would give STEP = 30./20. = 1.5 (day)
-real(8),parameter :: TMAX = 180 ! should be tested for a hundred years (3.65e4 days)
+real(8),parameter :: TMAX = 3.65e4 ! should be tested for a hundred years (3.65e4 days)
 
 end module data_parameters
