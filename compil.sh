7be43a78bc6ad32f3d393f459728fcfa701c2feb
Clément Robert <clement.robert@protonmail.com>
2015-11-02 23:21:21 +0100

    update compil.sh

--- compil.sh
+++ compil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python parser.py
-
+PYTERM="python"
+PARSER="parser.py"
 COMPILER="gfortran"
 TARGET="program"
 MAIN="main.f90"
@@ -10,6 +10,8 @@ SRC90="modules/maths.f90 modules/parameters.f90 modules/data_planets.f90 modules
 OUT="maths.o parameters.o data_planets.o sub_nbodies.o RA15M.o"
 WFLAGS="-Wall -Wextra  -fbounds-check"
 
+
+$PYTERM $PARSER
 $COMPILER -c $SRC77
 $COMPILER -c $SRC90
 $COMPILER $WFLAGS $MAIN $OUT -o $TARGET
@@ -17,5 +19,4 @@ $COMPILER $WFLAGS $MAIN $OUT -o $TARGET
 #clean
 rm *.o *.mod
 
-#cleaner (but not usable yet) way to declare modules :
-#SRC90=$(ls modules/*.f90)
+
