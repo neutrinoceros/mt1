a2f43d8a7b05027f24d28934a7eecf0e203a0937
Clément Robert <clement.robert@protonmail.com>
2015-11-12 18:48:56 +0100

    code formating + addition of vel.dat and vel_back.dat output files

--- main.f90
+++ main.f90
@@ -3,21 +3,38 @@ program ephemerids
 use maths
 use sub_nbodies
 use parameters
-use data_planets !initial conditions + masses
+use data_planets ! initial conditions + masses
+
+
+!================================================================
+!                    variables declaration
+!================================================================
 
 implicit none
+
 real(8),dimension(:),allocatable :: Positions, Velocities
-integer :: i!,j
+integer :: i
 real(8) :: itime, ftime
 real(8) :: Etot, Ltot
-character(len=30) :: OFMT1,OFMT2,OFMT3 !line format for diverse out files
 
-!spice useful variables
-real(8) :: ET,LT,date_d !ET is ..., LT is light-time, date_d is time passed since init_date_jd in days 
+! line formats for out files
+!----------------------------
+character(len=30) :: OFMT1,OFMT2,OFMT3 
+
+!  SPICE useful variables
+!----------------------------
+real(8) :: ET,LT,date_d 
 real(8),dimension(6) :: body_state
+! ET is ...
+! LT is light-time
+! date_d is time passed since init_date_jd in days 
+! body_state is pos/vel of one body ; tmp variable
 
+!        allocation
+!----------------------------
 allocate(Positions(3*N_BOD))
 allocate(Velocities(3*N_BOD))
+
 
 !================================================================
 !                       init, file opening...
@@ -31,13 +48,16 @@ call AMomentum(Positions, Velocities, ftime, Ltot)
 itime = 0.
 ftime = SSTEP
 OFMT1 = "(3E30.16E3)"   ! fmt of ipms.dat and imps_back.dat
-OFMT2 = "(34E30.16E3)"  ! fmt of traj.dat and traj_back.dat
+OFMT2 = "(34E30.16E3)"  ! fmt of traj.dat, traj_back.dat, 
+                        !        vel.dat, vel_back.dat
 OFMT3 = "(7E30.16E3)"   ! fmt of traj_spice.dat
 
-open(10,file='results/ipms.dat',status='replace')         ! intégrales premières
-open(11,file='results/ipms_back.dat',status='replace')    ! intégrales premières, au retour
-open(20,file='results/traj.dat',status='replace')         ! positions
-open(21,file='results/traj_back.dat',status='replace')    ! positions, au retour
+open(10,file='results/ipms.dat'        ,status='replace')  ! intégrales premières
+open(11,file='results/ipms_back.dat'   ,status='replace')  ! intégrales premières, au retour
+open(20,file='results/traj.dat'        ,status='replace')  ! positions
+open(21,file='results/traj_back.dat'   ,status='replace')  ! positions, au retour
+open(30,file='results/vel.dat'         ,status='replace')  ! velocities
+open(31,file='results/vel_back.dat'    ,status='replace')  ! velocities, au retour
 open(16,file='results/out_everhart.dat',status='replace')
 
 write(10,*) "#     time                         Etot                           Ltot"
@@ -53,11 +73,16 @@ print*, 'bodies used are : ', NAMES
 !================================================================
 
 print*, "Entering main loop."
+
+print*, "Starting forward integration..."
+!-----------------------------------------
+
 i=0
 do while (itime < TMAX)
    i = i+1
    if (mod(i,int(SAMPLERATE)) .eq. 0) then 
       write(20,OFMT2) itime, Positions
+      write(30,OFMT2) itime, Velocities
    end if
    call walk(Positions, Velocities, itime, ftime)
    call Energy(Positions, Velocities, ftime, Etot)
@@ -68,9 +93,14 @@ do while (itime < TMAX)
 end do
 print *, "TMAX reached."
 
-!----------------------------------------------------------------
+close(10)
+close(20)
+close(30)
 
-print*, "Back to starting point."
+
+print*, "Starting backward integration..."
+!-----------------------------------------
+
 i=0
 itime = ftime
 ftime = itime - SSTEP
@@ -78,6 +108,7 @@ do while (ftime .ge. 0)
    i = i+1
    if (mod(i,int(SAMPLERATE)) .eq. 0) then 
       write(21,OFMT2) itime, Positions
+      write(31,OFMT2) itime, Velocities
    end if
    call walk(Positions, Velocities, itime, ftime)
    call Energy(Positions, Velocities, ftime, Etot)
@@ -87,10 +118,13 @@ do while (ftime .ge. 0)
    ftime = ftime - SSTEP
 end do
 write(21,OFMT2) itime, Positions
+write(31,OFMT2) itime, Velocities
+print*, "T=0 reached."
 
-close(10)
-close(20)
-close(30)
+close(11)
+close(21)
+close(31)
+
 close(16)
 
 
